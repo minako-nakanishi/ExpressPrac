dfe720ef0e8e69de60e8bdf96287beab5b91f229
Minako Nakanishi <n135216e@gmail.com>
2019-03-14 14:49:08 +0900

    JSONでリクエストを送るよう設定

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const express = require('express');
 // expressアプリを生成する
 const app = express();
 
-// ルートパスに接続されたときに、コンソールにLOGGERと出力させる.
-var myLogger = function(req,res,next){
-    console.log('LOGGED');
-    next(); //関数の実行(app.useで指定した関数を実行する)
-};
-
-app.use(myLogger); //myLogger関数を実行
-
-// ルート(http://localhost/)にアクセスしてきたときにHELLOと返す.
-app.get('/',function(req,res){
-    res.send('HELLO')}
-    );
+// ルート(http://localhost:3000/api/vi/list)にアクセスしてきたときにTODOリストを返す.
+app.get('/api/vi/list',function(req,res){
+    // クライアントに送るJSON
+    const todoList = [
+        {title:'JavaScriptを勉強する', done:true},
+        {title:'Node JSを勉強にする', done:false},
+        {title:'WebAPIを作成する', done:false}
+    ];
+    res.json(todoList); //JSONを送信する
+});
 
 
 
